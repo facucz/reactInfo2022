a521db281ee3430a8c46f94ad97c557135a1a307
fzaragoza <facundochristianzaragoza@gmail.com>
2022-07-16 14:57:27 -0300

    Added cypress library. Added Footer component tests

---
+++ cypress.config.js
@@ -0,0 +1,11 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  component: {
+    devServer: {
+      framework: "create-react-app",
+      bundler: "webpack",
+    },
+    specPattern: "**/*.test.{js,jsx,ts,tsx}"
+  },
+});
---
+++ cypress/component/Footer.test.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { mount, unmount } from 'cypress/react';
+import Footer from '../../src/components/Footer';
+
+describe('Footer component test', () => {
+
+  beforeEach(() => {
+    mount(<Footer></Footer>);
+  });
+
+  it('should contains Copyright', () => {
+    cy.contains('Copyright 2022').should('be.visible');
+  });
+
+  it('should contains Siguenos', () => {
+    cy.contains('Siguenos:').should('be.visible');
+  });
+
+  it('should contains Informatio text', () => {
+    cy.contains('Hecho para Informatorio - Resistencia - Chaco - 2022').should('be.visible');
+  });
+
+  it('should twitter icon be visible', () => {
+    const twitterIconSelector = '.bi-twitter';
+
+    cy.get(twitterIconSelector).should('be.visible');
+  });
+
+  it('should facebook icon be visible', () => {
+    const facebookIconSelector = '.bi-facebook';
+
+    cy.get(facebookIconSelector).should('be.visible');
+  });
+
+  it('should instagram icon be visible', () => {
+    const instagramIconSelector = '.bi-instagram';
+
+    cy.get(instagramIconSelector).should('be.visible');
+  });
+
+})
